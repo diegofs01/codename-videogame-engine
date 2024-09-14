952253fb43341e7722b660f02879c478bf95813f
Diego Ferreira <diegofs01@hotmail.com>
2024-09-14 11:08:47 -0300

    fix bash script

--- broco.sh
+++ broco.sh
@@ -8,15 +8,15 @@ fi
 case $1 in
 
     love | lua)
-        ./cli.sh build ./broco/game.lua --core love --run
+        ./cli.sh build ./examples/brocos/game.lua --core love --run
     ;;
 
     html | html5)
-        ./cli.sh build ./broco/game.lua --core html5
+        ./cli.sh build ./examples/brocos/game.lua --core html5
     ;;
 
     ginga)
-        # ./cli.sh build ./broco/game.lua --core love --run
+        # ./cli.sh build ./examples/brocos/game.lua --core love --run
     ;;
 
     *)
